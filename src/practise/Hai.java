f156d2c89e15b764762eb0e7ca1ef6149d1c973d
praveen <praveenkandukuru07@gmail.com>
2024-01-06 09:39:17 +0530

    This is my first commit for Advance selenium

---
+++ src/practise/Hai.java
@@ -0,0 +1,16 @@
+package practise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hai {
+ public static void main(String[] args) {
+	 System.setProperty("webdriver.chrome.driver", "D:\\selenium\\chromedriver_win32\\chromedriver.exe");
+	 WebDriver driver=new ChromeDriver();
+	 driver.manage().window().maximize();
+	 driver.get("https://demo.actitime.com/login.do");
+	 
+	
+}
+
+}
